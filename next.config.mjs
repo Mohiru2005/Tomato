232c649ac5ac4bb8cb83328708e6a7c0c8426920
Mohiru2005 <2300031066@kluniversity.in>
2026-08-23 17:26:37 +0530

    Add automatic Next.js redirect from Vercel to Render app

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  async redirects() {
+    if (process.env.VERCEL) {
+      return [
+        {
+          source: '/:path*',
+          destination: 'https://tomato-26nw.onrender.com/:path*',
+          permanent: false,
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
